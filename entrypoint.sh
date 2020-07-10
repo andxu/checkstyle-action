65b69bb7d08f38d2918613ca50c76a922d746c11
Andy Xu(devdiv) <andxu@microsoft.com>
2020-07-10 22:08:00 +0800

    Develop (#1)
    * Use java checkstyle by andxu

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,12 @@ echo "Running check"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-exec java -jar /checkstyle.jar -c /"${INPUT_CHECKSTYLE_CONFIG}" "${INPUT_WORKDIR}" -f xml \
+git fetch origin ${GITHUB_BASE_REF}
+git diff-tree -r --no-commit-id --name-only ${GITHUB_SHA} origin/${GITHUB_BASE_REF} > changes.txt
+echo "changes.txt generated"
+node /java-checkstyle/src/checkstyle /github/workspace
+echo "checkstyle-result.xml generated"
+exec cat /github/workspace/checkstyle-result.xml \
  | reviewdog -f=checkstyle \
       -name="${INPUT_TOOL_NAME}" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
